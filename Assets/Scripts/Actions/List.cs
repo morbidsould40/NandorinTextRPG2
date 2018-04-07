b82abbb857d1807b73c1a44cc8e57c672a724ded
Unknown <rschumaker40@gmail.com>
2018-04-06 23:39:26 -0400

    You can now type list to see weapons and armor sold in the shops.

---
+++ Assets/Scripts/Actions/List.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextRPG/InputActions/List")]
+public class List : InputAction
+{
+
+    public override void RespondToInput(GameController controller, string[] seperatedInputWords)
+    {
+        if (controller.roomNavigation.currentRoom.isShop)
+        {
+            Dictionary<string, int> storeInventory = new Dictionary<string, int>();
+            var shopWeapons = controller.roomNavigation.currentRoom.shopWeapons;
+            var shopArmor = controller.roomNavigation.currentRoom.shopArmor;
+            if (shopWeapons.Length > 0)
+            {
+                for (int i = 0; i < shopWeapons.Length; i++)
+                {
+                    storeInventory.Add(shopWeapons[i].weaponName, shopWeapons[i].weaponCost);
+                }
+            }
+            if (shopArmor.Length > 0)
+            {
+                for (int i = 0; i < shopArmor.Length; i++)
+                {
+                    storeInventory.Add(shopArmor[i].armorName, shopArmor[i].armorCost);
+                }
+            }
+            controller.LogStringWithReturn("For Sale\n_______________________________________________");
+            foreach (var item in storeInventory)
+            {
+                controller.LogStringWithReturn(item.Key + ": " + item.Value + " gold pieces");
+            }
+            controller.DisplayLoggedText();
+            storeInventory.Clear();
+        }
+        else
+        {
+            controller.LogStringWithReturn("You must be in a shop to see a LIST of wares.");
+            controller.DisplayLoggedText();
+        }
+    }
+}
